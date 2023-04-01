5715346c7e8a2e6ea976bfe4ce7e3043529db6b5
PedroReoli <128660097+PedroReoli@users.noreply.github.com>
2023-04-01 18:37:42 -0300

    att 01/04

--- trabalhos/HTML/JS/car.js
+++ trabalhos/HTML/JS/car.js
@@ -1,49 +1,82 @@
-// Recuperar itens do carrinho do localStorage ou criar um array vazio
-let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+const produtosSelecionados = JSON.parse(localStorage.getItem('produtos'));
+console.log(produtosSelecionados);
+let mensagem = 'Produtos selecionados:\n';
 
-// Função para exibir os itens no carrinho
-function displayCartItems() {
-  const cartTable = document.createElement('table');
-  const headerRow = cartTable.insertRow();
-  headerRow.innerHTML = '<th>Produto</th><th>Preço</th>';
-
-  let totalPrice = 0;
-  cartItems.forEach(item => {
-    const itemRow = cartTable.insertRow();
-    const nameCell = itemRow.insertCell(0);
-    nameCell.textContent = item.name;
-    const priceCell = itemRow.insertCell(1);
-    priceCell.textContent = item.price;
-    totalPrice += Number(item.price.replace(/[^0-9.-]+/g,"")); // converte o preço em um número e adiciona ao total
+if (produtosSelecionados && produtosSelecionados.length > 0) {
+  produtosSelecionados.forEach(produto => {
+    mensagem += '- ' + produto.nome + ' (' + produto.preco + ')\n';
   });
-
-  document.querySelector('#cart-table tbody').appendChild(cartTable);
-
-  // Atualiza o valor total da compra
-  document.querySelector('p').textContent = `Total da compra: R$ ${totalPrice.toFixed(2)}`;
+} else {
+  mensagem = 'Nenhum produto selecionado';
 }
 
-displayCartItems();
+alert(mensagem);
 
-// Limpar carrinho e atualizar itens do carrinho no localStorage
-function clearCart() {
-  cartItems = [];
-  localStorage.setItem('cartItems', JSON.stringify(cartItems));
-  displayCartItems();
-}
+// Seleciona o elemento da tabela onde serão inseridos os produtos
+const tabela = document.querySelector('#cart-table');
 
-// Adicionar evento de clique no botão de limpar carrinho
-const clearButton = document.querySelector('#clear-cart');
-clearButton.addEventListener('click', clearCart);
+// Adiciona uma linha para cada produto selecionado
+produtosSelecionados.forEach(produto => {
+  // Cria uma nova linha na tabela
+  const linha = document.createElement('tr');
+  
+  // Cria uma célula para o nome do produto
+  const celulaNome = document.createElement('td');
+  celulaNome.textContent = produto.nome;
+  
+  // Cria uma célula para o preço do produto
+  const celulaPreco = document.createElement('td');
+  celulaPreco.textContent = produto.preco;
+  
+  // Adiciona as células à linha
+  linha.appendChild(celulaNome);
+  linha.appendChild(celulaPreco);
+  
+  // Adiciona a linha à tabela
+  tabela.appendChild(linha);
+});
+// Calcula o total da compra
+const totalCompra = produtosSelecionados.reduce((total, produto) => total + parseFloat(produto.preco.replace(',', '.').substring(3)), 0);
 
-// Adicionar evento de clique no botão de finalizar compra
-const buyButton = document.querySelector('#buy-cart');
-buyButton.addEventListener('click', function() {
-  if (cartItems.length > 0) {
-    const totalPrice = cartItems.reduce((acc, item) => acc + Number(item.price.replace(/[^0-9.-]+/g,"")), 0);
-    const url = `checkout.html?totalPrice=${totalPrice}`;
-    window.location.href = url;
-  } else {
-    alert("Seu carrinho está vazio. Adicione itens para continuar com a compra.");
-  }
+// Seleciona o elemento do total da compra
+const totalCompraElemento = document.querySelector('p');
+
+// Exibe o total da compra no elemento selecionado
+totalCompraElemento.textContent = `Total da compra: R$ ${totalCompra.toFixed(2).replace('.', ',')}`;
+// TESTANDO 
+
+document.querySelector('#buy-cart').addEventListener('click', () => {
+  const w = window.open('', '_blank', 'width=800,height=600');
+  w.document.write(`
+    <html>
+      <head>
+        <title>Finalizar Compra</title>
+        <link rel="stylesheet" href="/trabalhos/CSS/car.css">
+      </head>
+      <body>
+        <h1>Finalizar Compra</h1>
+        <form>
+          <label for="name">Nome:</label>
+          <input type="text" id="name" name="name"><br><br>
+          
+          <label for="email">E-mail:</label>
+          <input type="email" id="email" name="email"><br><br>
+          
+          <label for="address">Endereço:</label>
+          <textarea id="address" name="address"></textarea><br><br>
+          
+          <label for="card-number">Número do Cartão:</label>
+          <input type="text" id="card-number" name="card-number"><br><br>
+          
+          <label for="card-expiry">Data de Expiração:</label>
+          <input type="text" id="card-expiry" name="card-expiry" placeholder="MM/YY"><br><br>
+          
+          <label for="card-cvv">CVV:</label>
+          <input type="text" id="card-cvv" name="card-cvv"><br><br>
+          
+          <button type="submit">Confirmar Compra</button>
+        </form>
+      </body>
+    </html>
+  `);
 });
--- trabalhos/HTML/JS/script.js
+++ trabalhos/HTML/JS/script.js
@@ -1,89 +1,27 @@
-let count = 1;
-document.getElementById("radio1").checked = true;
+// Cria um array vazio para armazenar os produtos
+const produtos = [];
 
-setInterval( function(){
-  nextImage();
-}, 5000)
+// Seleciona todos os botões de compra
+const botoesCompra = document.querySelectorAll('.buy-button');
 
-function nextImage(){
-  count++;
-  if (count>3){
-    count = 1;
-  }
+// Adiciona um listener de clique em cada botão de compra
+botoesCompra.forEach(botao => {
+  botao.addEventListener('click', () => {
+    // Seleciona o nome e preço do produto correspondente
+    const nome = botao.parentNode.querySelector('h3').textContent;
+    const preco = botao.parentNode.querySelector('.price').textContent;
 
-  document.getElementById("radio"+count).checked = true;
+    // Adiciona o produto ao array
+    produtos.push({ nome, preco });
 
+// Verifica se o produto foi adicionado com sucesso e exibe o alerta
+if (produtos.length > 0 && produtos[produtos.length - 1].nome === nome && produtos[produtos.length - 1].preco === preco) {
+  alert("Produto adicionado");
 }
+// Armazena os produtos no local storage
+localStorage.setItem('produtos', JSON.stringify(produtos));
 
-
-
-
-
-
-const buyButtons = document.querySelectorAll(".buy-button"); // seleciona todos os botões de compra
-let cartItems = JSON.parse(localStorage.getItem('cartItems')) || []; // recupera os itens do carrinho salvos no localStorage, ou cria um array vazio
-
-// define a função que será executada quando o botão "Comprar" for clicado
-function addToCart(event) {
-  const product = event.target.parentElement; // seleciona o elemento pai do botão "Comprar", que é o item da lista de produtos
-  const productName = product.querySelector("h3").textContent; // seleciona o nome do produto
-  const productPrice = product.querySelector(".price").textContent; // seleciona o preço do produto
-
-  const cartItem = { // cria um objeto com os dados do produto
-    name: productName,
-    price: productPrice
-  };
-
-  cartItems.push(cartItem); // adiciona o objeto à lista de itens do carrinho
-
-  // salva os itens do carrinho no localStorage
-  localStorage.setItem('cartItems', JSON.stringify(cartItems));
-
-  alert("Item adicionado ao carrinho:", cartItem);
-
-  // Exibe os itens no carrinho
-  const cartTable = document.createElement('table');
-  const headerRow = cartTable.insertRow();
-  headerRow.innerHTML = '<th>Produto</th><th>Preço</th>';
-
-  let totalPrice = 0;
-  cartItems.forEach(item => {
-    const itemRow = cartTable.insertRow();
-    const nameCell = itemRow.insertCell(0);
-    nameCell.textContent = item.name;
-    const priceCell = itemRow.insertCell(1);
-    priceCell.textContent = item.price;
-    totalPrice += Number(item.price.replace(/[^0-9.-]+/g,"")); // converte o preço em um número e adiciona ao total
+    // Imprime o array de produtos no console para verificar
+    console.log(produtos);
   });
-
-  document.querySelector('#cart-table tbody').appendChild(cartTable);
-
-  //atualiza o valor total da compra 
-  document.querySelector('p').textContent = `Total da compra: R$ ${totalPrice.toFixed(2)}`;
-}
-
-// adiciona o evento de clique a cada botão de compra
-buyButtons.forEach(button => {
-  button.addEventListener("click", addToCart);
 });
-
-// exibe os itens no carrinho
-const cartTable = document.createElement('table');
-const headerRow = cartTable.insertRow();
-headerRow.innerHTML = '<th>Produto</th><th>Preço</th>';
-
-let totalPrice = 0;
-cartItems.forEach(item => {
-  const itemRow = cartTable.insertRow();
-  const nameCell = itemRow.insertCell(0);
-  nameCell.textContent = item.name;
-  const priceCell = itemRow.insertCell(1);
-  priceCell.textContent = item.price;
-  totalPrice += Number(item.price.replace(/[^0-9.-]+/g,"")); // converte o preço em um número e adiciona ao total
-});
-
-document.querySelector('#cart-table tbody').appendChild(cartTable);
-
-//atualiza o valor total da compra 
-document.querySelector('p').textContent = `Total da compra: R$ ${totalPrice.toFixed(2)}`;
-
